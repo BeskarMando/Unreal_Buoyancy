38c5fee9ed57280a873f3bab5ee23a2b6ea53c46
Tobias Moos <37347326+BeskarMando@users.noreply.github.com>
2021-05-17 19:13:17 -0400

    Update BuoyantMeshComponent.h

--- Buoyancy/PawnSystem/BuoyantMeshComponent.h
+++ Buoyancy/PawnSystem/BuoyantMeshComponent.h
@@ -2,7 +2,7 @@
 * FileName: BuoyantMeshComponent.h
 *
 * Created by: Tobias Moos
-* Project name: Sails of War / OceanProject
+* Project name: Sails of War
 * Unreal Engine version: 4.19
 * Created on: 2020/01/08
 *
@@ -17,7 +17,6 @@
 *
 * Feel free to use this software in any commercial/free game.
 * Selling this as a plugin/item, in whole or part, is not allowed.
-* See "OceanProject\License.md" for full licensing details.
 * =================================================*/
 #pragma once
 
